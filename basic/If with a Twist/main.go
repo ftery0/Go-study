226695d27bc45d1acf9927bc89d57976edc6d026
해준 <haejun7388@gmail.com>
2026-01-28 22:21:26 +0900

    If with a Twist, switch

---
+++ basic/If with a Twist/main.go
@@ -0,0 +1,15 @@
+package main
+import "fmt"
+
+func canIDrink(age int) bool {
+	if koreanAge := age + 2; koreanAge < 18 {
+		return false
+
+	}
+	return true
+}
+
+func main() {
+	fmt.Println(canIDrink(16))
+	fmt.Println(canIDrink(18))
+}
---
+++ basic/If with a Twist/switch.go
@@ -0,0 +1,13 @@
+package main
+
+
+func switchExample(age int) string {
+	switch age {
+	case 10:
+		return "false"
+	case 20:
+		return "true"
+	default:
+		return "false"
+	}
+}
